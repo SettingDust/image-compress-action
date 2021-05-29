111035ebc4950ac715994131d62c1ba9b7d79b9f
SettingDust <settingdust@gmail.com>
2021-05-29 17:27:49 +0800

    添加尺寸

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import globby from 'globby'
 import imagemin from 'imagemin'
 import imageminMozjpeg from 'imagemin-mozjpeg'
 import imageminPngquant from 'imagemin-pngquant'
+import sharp from 'sharp'
+import {imageSize} from 'image-size'
 
 async function compress(
   source: string,
@@ -34,29 +36,45 @@ async function run(): Promise<void> {
   const quality: number | undefined =
     parseInt(core.getInput('quality')) || undefined
   const minSize: number = parseInt(core.getInput('min-size'))
+  const maxDimension: number = parseInt(core.getInput('max-dimension'))
 
   let fileArray = files.split('⭐')
   console.log(`输入文件：${fileArray}`)
-  if (minSize) {
-    fileArray = fileArray
-      .map(it => path.resolve(it))
-      .filter(it => statSync(it).size > minSize)
-  }
+  fileArray = fileArray.map(it => path.resolve(it))
 
   const filePaths = await globby(fileArray, {onlyFiles: true})
   const outputs: string[] = []
 
-  for (const file of filePaths) {
+  for (let file of filePaths) {
     const size = statSync(file).size
     const realQuality: number = quality ? quality : (minSize / size) * 100
+    let dimension = imageSize(file)
     console.log(`正在处理：${file}`)
     console.log(`压缩前大小：${size / 1024} KB`)
+    console.log(`压缩前尺寸 ：${dimension.width} x ${dimension.height}`)
     console.log(`质量：${realQuality}`)
 
-    const result = await compress(file, realQuality, minSize)
+    if (
+      maxDimension &&
+      dimension.width &&
+      dimension.height &&
+      (dimension.height > maxDimension || dimension.width > maxDimension)
+    ) {
+      await sharp(file)
+        .resize(maxDimension, maxDimension, {
+          fit: 'inside'
+        })
+        .toFile((file += '.resized.jpg'))
+    }
 
-    console.log(`压缩后大小：${statSync(path.resolve(result)).size / 1024} KB`)
-    outputs.push(result)
+    if (size > minSize) {
+      file = await compress(file, realQuality, minSize)
+    }
+    dimension = imageSize(file)
+
+    console.log(`压缩后大小：${statSync(path.resolve(file)).size / 1024} KB`)
+    console.log(`压缩后尺寸 ：${dimension.width} x ${dimension.height}`)
+    outputs.push(file)
   }
   core.setOutput('images', outputs.join('⭐'))
 }
