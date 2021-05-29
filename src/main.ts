e12a537a198ff16b79972926b0ccdcfc51f139fb
SettingDust <settingdust@gmail.com>
2021-05-29 12:10:43 +0800

    Use imagemin

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import * as core from '@actions/core'
-import {statSync, writeFileSync} from 'fs'
+import {statSync} from 'fs'
 import globby from 'globby'
-import {ImagePool} from '@squoosh/lib'
-import * as path from 'path'
+import imagemin from 'imagemin'
+import imageminMozjpeg from 'imagemin-mozjpeg'
+import imageminPngquant from 'imagemin-pngquant'
 
 async function run(): Promise<void> {
   const files: string = core.getInput('files')
@@ -17,29 +18,26 @@ async function run(): Promise<void> {
 
   const filePaths = await globby(fileArray, {onlyFiles: true})
   const outputs: string[] = []
-  const imagePool: ImagePool = new ImagePool()
 
-  for (const fileName of filePaths) {
-    const file = path.resolve(__dirname, fileName)
+  for (const file of filePaths) {
     const realQuality: number = quality
       ? quality
       : (minSize / statSync(file).size) * 100
-    const image = imagePool.ingestImage(file)
-    image
-      .encode({
-        mozjpeg: {
-          quality: realQuality
-        }
-      })
-      .then(async () => {
-        const encodedImage = await image.encodedWith['mozjpeg']
-        const filename =
-          file.slice(0, file.lastIndexOf('.') + 1) +
-          (await encodedImage).extension
-        writeFileSync(filename, (await encodedImage).binary)
-        outputs.push(filename)
-        core.setOutput('images', outputs.join('\n'))
-      })
+    imagemin([file], {
+      destination: 'build/images',
+      plugins: [
+        imageminMozjpeg({
+          quality: Math.floor(realQuality)
+        }),
+        imageminPngquant({
+          quality: [0.0, realQuality / 100]
+        })
+      ]
+    })
+      .then(res => outputs.push(res.map(it => it.destinationPath)[0]))
       .catch(reson => console.log(reson))
   }
+  core.setOutput('images', outputs.join('\n'))
 }
+
+run()
